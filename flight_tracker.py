851c12c039484ee5871269e1f9a2631b91515ed3
Kyle Kent <kylekent@Kyles-MacBook-Pro-5.local>
2020-09-16 16:45:59 -0600

    A real-time flight tracker

---
+++ flight_tracker.py
@@ -0,0 +1,78 @@
+# importing modules
+import requests
+import json
+import pandas as pd
+import numpy as np
+from bokeh.plotting import figure, show
+from bokeh.tile_providers import get_provider, STAMEN_TERRAIN
+from bokeh.models import HoverTool, LabelSet, ColumnDataSource
+
+# WGS84 coordinate range of the US.
+lon_min, lat_min = -125.974, 30.038
+lon_max, lat_max = -68.748, 52.214
+
+# Querying the open sky network API
+user_name = ''
+password = ''
+url_data = 'https://' + user_name + ':' + password + '@opensky-network.org/api/states/all?' + 'lamin=' + str(
+    lat_min) + '&lomin=' + str(lon_min) + '&lamax=' + str(lat_max) + '&lomax=' + str(lon_max)
+response = requests.get(url_data).json()
+
+# convert json html data to a df
+col_name = ['icao24', 'callsign', 'origin_country', 'time_position', 'last_contact', 'long', 'lat', 'baro_altitude',
+            'on_ground', 'velocity', 'true_track', 'vertical_rate', 'sensors', 'geo_altitude', 'squawk', 'spi',
+            'position_source']
+flight_df = pd.DataFrame(response['states'], columns=col_name)
+flight_df = flight_df.fillna('No Data')  # replace NAN with No Data
+flight_df.head()
+
+
+# Defining function to convert WGS84 coordinates to web mercator point
+def wgs84_web_mercator_point(lon, lat):
+    k = 6378137
+    x = lon * (k * np.pi / 180)
+    y = np.log(np.tan((90 + lat) * np.pi / 360)) * k
+    return x, y
+
+# converting flight_df coordinates to web mercator
+def wgs84_to_web_mercator(df, lon="long", lat="lat"):
+    k = 6378137
+    df["x"] = df[lon] * (k * np.pi / 180.0)
+    df["y"] = np.log(np.tan((90 + df[lat]) * np.pi / 360.0)) * k
+    return df
+
+
+# converting coordinates
+xy_min = wgs84_web_mercator_point(lon_min, lat_min)
+xy_max = wgs84_web_mercator_point(lon_max, lat_max)
+
+wgs84_to_web_mercator(flight_df)
+flight_df['rot_angle'] = flight_df['true_track'] * -1
+icon_url = 'https://.....'
+flight_df['url'] = icon_url
+
+x_range, y_range = ([xy_min[0], xy_max[0]], [xy_min[1], xy_max[1]])
+p = figure(x_range=x_range, y_range=y_range, x_axis_type='mercator', y_axis_type='mercator', sizing_mode='scale_width',
+           plot_height=300)
+
+# map framework
+flight_source = ColumnDataSource(flight_df)
+tile_prov = get_provider(STAMEN_TERRAIN)
+p.add_tile(tile_prov, level='image')
+p.image_url(url='url', x='x', y='y', source=flight_source, anchor='center', angle_units='deg', angle='rot_angle',
+            h_units='screen', w_units='screen', w=40, h=40)
+# plotting the flights
+p.circle('x', 'y', source=flight_source, fill_color='red', hover_color='yellow', size=10, fill_alpha=0.8, line_width=0)
+
+# get a label for each point when hovered over
+my_hover = HoverTool()
+my_hover.tooltips = [('Call sign', '@callsign'), ('Origin Country', '@origin_country'), ('velocity(m/s)', '@velocity'),
+                     ('Altitude(m)', '@baro_altitude')]
+labels = LabelSet(x='x', y='y', text='callsign', level='glyph',
+                  x_offset=5, y_offset=5, source=flight_source, render_mode='canvas', background_fill_color='white',
+                  text_font_size="8pt")
+p.add_tools(my_hover)
+p.add_layout(labels)
+
+# show the map
+show(p)
